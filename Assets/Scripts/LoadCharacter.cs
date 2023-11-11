805bb836fef491eb522f455951124f013fdef34b
Stephanie Takeme <stephanie.takeme@gmail.com>
2023-11-11 08:27:16 +0100

    invisible barriers and Player selection
    Added invisible barriers to prevent the player from falling off the screen. Added a character selection scene for player to be able to select their fighter.

---
+++ Assets/Scripts/LoadCharacter.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class LoadCharacter : MonoBehaviour
+{
+    public GameObject[] charPrefabs;
+    public Transform SpawnPoint;
+    private int selectedCharacter = 0; // have a default selected character index
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Load up the selected player character
+        selectedCharacter = PlayerPrefs.GetInt("selectedCharacter"); // get the selected char index
+        GameObject prefab = charPrefabs[selectedCharacter];
+        // instantiate an instance of the selected character
+        GameObject player = Instantiate(prefab, prefab.transform.position, prefab.transform.rotation);
+        player.SetActive(true); //ACtivate character instance
+        player.name = "Player";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
